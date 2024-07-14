d1f52f2038bcfdc49b5594d45ca231a29b993936
AkmalXYZ/ <102523508+Akmal-XYZ@users.noreply.github.com>
2024-07-14 10:15:43 +0700

    Add files via upload

---
+++ wl.js
@@ -0,0 +1,13 @@
+document.getElementById('namaForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  
+  let inputNama = document.getElementById('namaInput').value;
+  let regex = /^(?!.*(?:Shelby|Tzy|Pcsi|Shellby|shellby|Tzy|Kontol|Memek|Ngentot|Puki|Jembut|Peler|Asu|tzy))(?=.*_)[a-zA-Z0-9_]+$/;
+  
+  if (!regex.test(inputNama)) {
+    alert("NAMA TIDAK VALID\nMungkin Nama Anda Mengandung Kata Terlarang");
+  } else {
+    let website = "https://wa.me/+6288214764360?text= √wl " + inputNama + "";
+    window.location.href = website;
+  }
+});
