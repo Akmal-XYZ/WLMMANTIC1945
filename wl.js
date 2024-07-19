8bc18a719f5f79c4f1bb5a521e42822680d55e3f
AkmalXYZ/ <102523508+Akmal-XYZ@users.noreply.github.com>
2024-07-19 14:58:01 +0700

    Update wl.js

--- wl.js
+++ wl.js
@@ -2,12 +2,17 @@ document.getElementById('namaForm').addEventListener('submit', function(e) {
   e.preventDefault();
   
   let inputNama = document.getElementById('namaInput').value;
-  let regex = /^(?!.*(?:Shelby|Tzy|Pcsi|Shellby|shellby|Tzy|Kontol|Memek|Ngentot|Puki|Jembut|Peler|Asu|tzy))(?=.*_)[a-zA-Z0-9_]+$/;
+  let regex = /^(?!.*(?:Shelby|Tzy|Pcsi|Shellby|shellby|Tzy|Kontol|Memek|Ngentot|Puki|Jemut|Peler|Asu|tzy|dajjal|Skibidi|Yesus|yesus|mewing|skibidi|cool|Cool|suki|Suki|Alpha|Sigma|sigma|ambatukam|Ambatukam|Amba|amba|Yanto_Cukurukuk|cold|Cold))(?=.*_)(?=(?:[a-z]*[A-Z]){0,2})[a-zA-Z0-9_]+$/;
   
   if (!regex.test(inputNama)) {
-    alert("NAMA TIDAK VALID\nMungkin Nama Anda Mengandung Kata Terlarang");
+    document.getElementById('error').innerText = "⚠️Masukin Nama Yang Bener Kontol";
   } else {
-    let website = "mailto: aditgaming1945gg@gmail.com?subject=wl " + inputNama + "";
-    window.location.href = website;
+    let uppercaseCount = inputNama.replace(/[^A-Z]/g, "").length;
+    if (uppercaseCount > 2) {
+      document.getElementById('error').innerText = "⚠️Masukin Nama Yang Bener Kontol";
+    } else {
+      let website = "mailto: aditgaming1945gg@gmail.com?subject=wl " + inputNama + "";
+      window.location.href = website;
+    }
   }
 });
