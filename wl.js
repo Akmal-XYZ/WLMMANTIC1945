d0786d7c56474c284cb029665b1429ac356f818a
AkmalXYZ/ <102523508+Akmal-XYZ@users.noreply.github.com>
2024-07-20 14:02:14 +0700

    Update wl.js

--- wl.js
+++ wl.js
@@ -1,18 +1,21 @@
 document.getElementById('namaForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-  
-  let inputNama = document.getElementById('namaInput').value;
+            e.preventDefault();
+            
+            let inputNama = document.getElementById('namaInput').value;
   let regex = /^(?!.*(?:Shelby|Tzy|Pcsi|Shellby|shellby|Tzy|Kontol|Memek|Ngentot|Puki|Jemut|Peler|Asu|tzy|dajjal|Skibidi|Yesus|yesus|mewing|skibidi|cool|Petot|petot|Patriot|patriotCool|suki|Suki|Alpha|Sigma|sigma|ambatukam|Ambatukam|Amba|amba|Yanto_Cukurukuk|cold|mantap|Mantap|Samp|samp|penis|Penis|gta|Gta|Fivem|fivem|cleo|Cleo|cheat|Cheat|Monet|monet|gaming|Gaming|Geming|Titit|titit|Ucay|ucay|Ambatron|ambatron|Cold))(?=.*_)(?=(?:[a-z]*[A-Z]){0,2})[a-zA-Z0-9_]+$/;
   
-  if (!regex.test(inputNama)) {
-    document.getElementById('error').innerText = "⚠️Masukin Nama Yang Bener Kontol";
-  } else {
-    let uppercaseCount = inputNama.replace(/[^A-Z]/g, "").length;
-    if (uppercaseCount > 2) {
-      document.getElementById('error').innerText = "⚠️Masukin Nama Yang Bener Kontol";
-    } else {
-      let website = "mailto: aditgaming1945gg@gmail.com?subject=wl " + inputNama + "&body=wl201945pcsi20202420mcrp20akmal20ytta222220whitelist.                                                                                                   JANGAN DI HAPUS WOI⚠️";
-      window.location.href = website;
-    }
-  }
-});
+            
+            if (!regex.test(inputNama)) {
+                document.getElementById('error').innerText = "⚠️Masukin Nama Yang Bener";
+            } else {
+                let uppercaseCount = inputNama.replace(/[^A-Z]/g, "").length;
+                if (uppercaseCount < 2) {
+                    document.getElementById('error').innerText = "⚠️Masukin Nama Yang Bener";
+                } else if (uppercaseCount > 2) {
+                    document.getElementById('error').innerText = "⚠️Hanya boleh ada 2 huruf besar dalam nama";
+                } else {
+                    let website = "mailto: aditgaming1945gg@gmail.com?subject=wl " + inputNama + "&body=wl201945pcsi20202420mcrp20akmal20ytta222220whitelist";
+                    window.location.href = website;
+                }
+            }
+        });
