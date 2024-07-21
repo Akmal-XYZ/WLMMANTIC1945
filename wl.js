acab290a1ceef8e3fd3b05fbc83b8aaa1ba49445
AkmalXYZ/ <102523508+Akmal-XYZ@users.noreply.github.com>
2024-07-21 13:18:13 +0700

    Update wl.js

--- wl.js
+++ wl.js
@@ -2,7 +2,7 @@ document.getElementById('namaForm').addEventListener('submit', function(e) {
             e.preventDefault();
             
             let inputNama = document.getElementById('namaInput').value;
-  let regex = /^(?!.*(?:Shelby|Tzy|Pcsi|Shellby|shellby|Tzy|Kontol|Memek|psuter|Psuter|Sotot|sotot|Ngentot|Puki|Jemut|Peler|Asu|tzy|dajjal|Skibidi|Yesus|yesus|mewing|skibidi|cool|Petot|petot|Patriot|patriotCool|suki|Suki|Alpha|Sigma|sigma|ambatukam|Ambatukam|Amba|amba|Yanto_Cukurukuk|cold|mantap|Mantap|Samp|samp|penis|Penis|gta|Gta|Fivem|fivem|cleo|Cleo|cheat|Cheat|Monet|monet|gaming|Gaming|Geming|Titit|titit|Ucay|ucay|Ambatron|ambatron|Cold))(?=.*_)(?=(?:[a-z]*[A-Z]){0,2})[a-zA-Z0-9_]+$/;
+  let regex = /^(?!.*(?:Shelby|Tzy|Pcsi|Shellby|shellby|Tzy|Ronaldo|ronaldo|Messi|messi|Yatim|yatim|telor|telur|Telor|telor|rendang|Rendang|Kontol|Memek|psuter|Psuter|Sotot|sotot|Ngentot|Puki|Jemut|Peler|Asu|tzy|dajjal|Skibidi|Yesus|yesus|mewing|skibidi|cool|Petot|petot|Patriot|patriotCool|suki|Suki|Alpha|Sigma|sigma|ambatukam|Ambatukam|Amba|amba|Yanto_Cukurukuk|cold|mantap|Mantap|Samp|samp|penis|Penis|gta|Gta|Fivem|fivem|cleo|Cleo|cheat|Cheat|Monet|monet|gaming|Gaming|Geming|Titit|titit|Ucay|ucay|Ambatron|ambatron|Cold))(?=.*_)(?=(?:[a-z]*[A-Z]){0,2})[a-zA-Z0-9_]+$/;
   
             
             if (!regex.test(inputNama)) {
@@ -14,7 +14,7 @@ document.getElementById('namaForm').addEventListener('submit', function(e) {
                 } else if (uppercaseCount > 2) {
                     document.getElementById('error').innerText = "⚠️Masukin Nama Yang Bener Ngentot";
                 } else {
-                    let website = "mailto: aditgaming1945gg@gmail.com?subject=wl " + inputNama + "&body=wl201945pcsi20202420mcrp20akmal20ytta222220whitelist";
+                    let website = "mailto: aditgaming1945gg@gmail.com?subject=wl " + inputNama + "&body=wl201945pcsi20202420mcrp20akmal20ytta222220whitelist19282829191919272191911918292919191919199akmaLLL9999999999919181819191919181811";
                     window.location.href = website;
                 }
             }
